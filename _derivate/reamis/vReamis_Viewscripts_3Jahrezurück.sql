7f929aa1ac23887c4cba268b34b6ef068e2a06af
Nathan Thurnreiter <chanan@gmx.net>
2024-03-04 11:19:16 +0100

    Update vReamis_Viewscripts_3Jahrezurück.sql
    Changed from Oracle to SQL Anywhere script

--- _derivate/reamis/vReamis_Viewscripts_3Jahrezurück.sql
+++ _derivate/reamis/vReamis_Viewscripts_3Jahrezurück.sql
@@ -1,31 +1,31 @@
 --fibu und bilanzkat sind allgemein und nicht auf Jahre heruntergebrochen
 
---fibubuchung auf letzte >= heute minus 3 Jahre einschränken
+--fibubuchung auf letzte >= heute minus 3 Jahre einschrÃ¤nken
 CREATE or REPLACE VIEW vReamisfibubuchung AS
 SELECT * from fibubuchung
-WHERE jahr >= ((select to_char(SYSDATE,'YYYY') from dual)-3);
+WHERE jahr >= (select Dateformat(getcurrentdate(),'YYYY')-3);
 
---fibukonto1 auf letzte >= heute minus 3 Jahre einschränken
+--fibukonto1 auf letzte >= heute minus 3 Jahre einschrÃ¤nken
 CREATE or REPLACE VIEW vReamisfibukonto1 AS
 SELECT * from fibukonto1
-WHERE jahr >= ((select to_char(SYSDATE,'YYYY') from dual)-3);
+WHERE jahr >= (select Dateformat(getcurrentdate(),'YYYY')-3);
 
---fibujahr1 auf letzte >= heute minus 3 Jahre einschränken
+--fibujahr1 auf letzte >= heute minus 3 Jahre einschrÃ¤nken
 CREATE or REPLACE VIEW vReamisfibujahr1 AS
 SELECT * from fibujahr1
-WHERE jahr >= ((select to_char(SYSDATE,'YYYY') from dual)-3);
+WHERE jahr >= (select Dateformat(getcurrentdate(),'YYYY')-3);
 
---bilanztx auf letzte >= heute minus 3 Jahre einschränken
+--bilanztx auf letzte >= heute minus 3 Jahre einschrÃ¤nken
 CREATE or REPLACE VIEW vReamisbilanztx AS
 SELECT * from bilanztx
-WHERE jahr >= ((select to_char(SYSDATE,'YYYY') from dual)-3);
+WHERE jahr >= (select Dateformat(getcurrentdate(),'YYYY')-3);
 
---fibubudget auf letzte >= heute minus 3 Jahre einschränken
+--fibubudget auf letzte >= heute minus 3 Jahre einschrÃ¤nken
 CREATE or REPLACE VIEW vReamisfibubudget AS
 SELECT * from fibubudget
-WHERE jahr >= ((select to_char(SYSDATE,'YYYY') from dual)-3);
+WHERE jahr >= (select Dateformat(getcurrentdate(),'YYYY')-3);
 
---budgetprokonto auf letzte >= heute minus 3 Jahre einschränken
+--budgetprokonto auf letzte >= heute minus 3 Jahre einschrÃ¤nken
 CREATE or REPLACE VIEW vReamisbudgetprokonto AS
 SELECT * from budgetprokonto
-WHERE jahr >= ((select to_char(SYSDATE,'YYYY') from dual)-3);
+WHERE jahr >= (select Dateformat(getcurrentdate(),'YYYY')-3);
